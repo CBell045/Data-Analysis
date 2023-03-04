2d86dc168fe838dddcce493c7f0cb796fe55c2c2
CBell045 <bel19006@byui.edu>
2023-03-04 11:07:51 -0700

    finished

---
+++ README.md
@@ -0,0 +1,38 @@
+# Overview
+
+The purpose of this project was to analyze a car dataset and determine what a good price is for different makes, models, and odometer readings. 
+
+I got the data from [Kaggle](https://www.kaggle.com/austinreese/craigslist-carstrucks-data). The dataset is used car data from Craigslist. It contains 25 columns and 458,213 rows. 
+
+I am currently dreaming of buying a truck, and I want to make sure that when I do, I get a good car for a good price. 
+
+Below is a link to my software demo video.
+
+[Software Demo Video](https://youtu.be/RK8zG6oxRtI)
+
+# Data Analysis Results
+
+Question: What is a good price for a Toyota Tacoma with 100,000 miles on it?
+Answer: Typically, a Toyota Tacoma with 100,000 miles on it is listed for $20,000. Anything below $15,000 is a good deal.
+
+Question: What is an equivalent price for a Ford truck? 
+Answer: Fords tend to sell for a little more, at around $24,000. 
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+{Describe the programming language that you used and any libraries.}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Web Site Name](http://url.link.goes.here)
+* [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* Item 1
+* Item 2
+* Item 3
--- app.py
+++ app.py
@@ -1,89 +1,129 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import html
-from dash import dash_table
+from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output, State
-
-
-# load data
 import pandas as pd
-df = pd.read_csv('job_data.csv')
-# create the app and set the Bootstrap theme
+import plotly.express as px
+
+
+# Load the data
+df = pd.read_csv("vehicles_clean.csv")
+df = df[(df['price'] < 200000)&(df['price'] > 100)]
+df = df[(df['odometer'] < 500000)&(df['odometer'] > 10)]
+df = df.drop(columns='Unnamed: 0')
+
+# Define the app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-# define the layout
+# Define the layout
 app.layout = dbc.Container([
     html.Br(),
-    html.H3('Job Data'),
-    html.P('Filter the data by searching or sorting the table below:'),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        editable=True,
-        row_deletable=True,
-        style_cell={
-            'textAlign': 'center',
-            'whiteSpace': 'normal',
-            'fontFamily': 'Lato',
-            'fontSize': '13px',
-        },
-        style_data_conditional=[
-        {
-            'if': {'column_id': 'TOTAL', 'filter_query': '{TOTAL} > 10'},
-            'backgroundColor': '#1ABC9C',
-            'color': 'white',
-        },
-        {
-            'if': {'column_id': 'TOTAL', 'filter_query': '{TOTAL} > 5 && {TOTAL} <= 10'},
-            'backgroundColor': '#F8BF75',
-            'color': 'white'
-        },
-        {
-            'if': {'column_id': 'TOTAL', 'filter_query': '{TOTAL} <= 5'},
-            'backgroundColor': '#F28D82',
-            'color': 'white'
-        }
-        ],
-        sort_action='native',
+    html.H1("Vehicle Data"),
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='year-dropdown',
+                options=[{'label': str(int(year)), 'value': year} for year in df['year'].sort_values(ascending=False).unique()],
+                placeholder="Select a year",
+            ),
+        ], className='col-3'),
+        html.Div([
+            dcc.Dropdown(
+                id='manufacturer-dropdown',
+                options=[{'label': manufacturer, 'value': manufacturer} for manufacturer in df['manufacturer'].unique()],
+                placeholder="Select a manufacturer",
+            ),
+        ], className='col-3'),
+        html.Div([
+            dcc.Dropdown(
+                id='model-dropdown',
+                options=[{'label': model, 'value': model} for model in df['model'].unique()],
+                placeholder="Select a model",
+            ),
+        ], className='col-3'),
+        html.Div([
+            dcc.Dropdown(
+                id='type-dropdown',
+                options=[{'label': type, 'value': type} for type in df['type'].unique()],
+                placeholder="Select a type",
+            ),
+        ], className='col-3'),
+    ], className='row mt-4'),
+    dcc.Graph(id='price-vs-odometer'),
+    html.Div( 
+        id='table-container',
+        children=[
+            dash_table.DataTable(
+                id='table',
+                columns=[
+                    {"name": i, "id": i} for i in sorted(df.columns)
+                ],
+                style_cell={
+                    'textAlign': 'center',
+                    'whiteSpace': 'normal',
+                    'fontFamily': 'Lato',
+                    'fontSize': '13px',
+                },
+                page_current=0,
+                page_size=5,
+                page_action='custom'
+                
+            )
+        ]
     ),
-    
-    html.Button('Add Row', id='add-rows-button', className="btn btn-primary btn-sm", n_clicks=0),
+])
 
-], 
-fluid=True,
-)
-
+# Define the callbacks
 @app.callback(
     Output('table', 'data'),
-    Input('add-rows-button', 'n_clicks'),
-    Input('table', 'data'),
-    State('table', 'data'),
-    State('table', 'columns'),
-    prevent_initial_call=True
+    [Input('table', 'page_current'),
+     Input('table', 'page_size'),
+     Input('year-dropdown', 'value'),
+     Input('manufacturer-dropdown', 'value'),
+     Input('model-dropdown', 'value'),
+     Input('type-dropdown', 'value')]
 )
-def update_table(data, n_clicks, rows, columns):
-    if dash.callback_context.triggered[0]['prop_id'] == 'add-rows-button.n_clicks':
-        # If the 'add-rows-button' was clicked, add a new empty row to the table
-        rows.append({c['id']: '' for c in columns})
-    else:
-        # If the data in the table was updated, update the 'TOTAL' column for each row
-        for row in rows:
-            try:
-                row['TOTAL'] = sum([float(row[i]) for i in list(row.keys())[2:9]])
-            except:
-                row['TOTAL'] = 'NA'
-    return rows
+def update_table(page_current, page_size, year, manufacturer, model, type):
+    filtered_df = df.copy()
+    # Filter the table data
+    if year is not None:
+        filtered_df = filtered_df[filtered_df['year'] == year]
+    if manufacturer is not None:
+        filtered_df = filtered_df[filtered_df['manufacturer'] == manufacturer]
+    if model is not None:
+        filtered_df = filtered_df[filtered_df['model'] == model]
+    if type is not None:
+        filtered_df = filtered_df[filtered_df['type'] == type]
+    return filtered_df.iloc[page_current * page_size: (page_current + 1) * page_size].to_dict('records')
+    
 
+@app.callback(
+    Output('price-vs-odometer', 'figure'),
+    [Input('year-dropdown', 'value'),
+     Input('manufacturer-dropdown', 'value'),
+     Input('model-dropdown', 'value'),
+     Input('type-dropdown', 'value')]
+)
+def update_graph(year, manufacturer, model, type):
+    filtered_df = df.copy()
+    # Filter the data
+    if year is not None:
+        filtered_df = filtered_df[filtered_df['year'] == year]
+    if manufacturer is not None:
+        filtered_df = filtered_df[filtered_df['manufacturer'] == manufacturer]
+    if model is not None:
+        filtered_df = filtered_df[filtered_df['model'].str.startswith(model)]
+    if type is not None:
+        filtered_df = filtered_df[filtered_df['type'] == type]
+    fig = px.scatter(filtered_df, x="odometer", y="price", trendline="ols", trendline_options=dict(log_x=True), trendline_color_override="black")
+    fig.update_layout(title=f"Price vs Odometer ({len(filtered_df)} vehicles)")
+    return fig
 
-# @app.callback(
-#     Output("table", "selected_cells"),
-#     Output("table", "active_cell"),
-#     Input("table", "active_cell"),    
-# )
-# def clear(active_cell):
-#     return [], None
-
-
+# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+# TODO
+# Question: What is a good price for a {year} {manufacturer} {model} with {odometer} miles?
+# Combine the update graph and update table callbacks into one callback
